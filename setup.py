30ae2f0b97334afeb91f98692b886e88e5d37fd7
hexhowells <morganhowells314@gmail.com>
2022-07-23 16:43:31 +0100

    add speaker interface to hexi package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
         name='hexi',
         version='0.1',
-        packages=['hexi/core', 'hexi/core/intent', 'hexi/interfaces/motor', 'hexi/interfaces/camera', 'hexi/speech'],
+        packages=['hexi/core', 'hexi/core/intent', 'hexi/interfaces/motor', 'hexi/interfaces/camera', 'hexi/interfaces/speaker',  'hexi/speech'],
         package_data={'assets': ['audio/*.mp3', 'face/*.png', 'face/*.gif']}
 )
 
