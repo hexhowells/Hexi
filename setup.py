0922edb0d9a9ac2a99bd7ac533ec446a09d95bf1
hexhowells <morganhowells314@gmail.com>
2022-07-25 21:36:22 +0100

    remove uneeded folders in core

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
         name='hexi',
         version='0.1',
-        packages=['hexi/core', 'hexi/core/intent', 'hexi/interfaces/motor', 'hexi/interfaces/camera', 'hexi/interfaces/speaker', 'hexi/interfaces/display',  'hexi/speech'],
+        packages=['hexi/core/intent', 'hexi/interfaces/motor', 'hexi/interfaces/camera', 'hexi/interfaces/speaker', 'hexi/interfaces/display',  'hexi/speech'],
         package_data={'assets': ['audio/*.mp3', 'face/*.png', 'face/*.gif']}
 )
 
