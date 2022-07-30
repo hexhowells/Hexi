1fbcf426ce725a4c4ec84bcc4ba92e381478692a
hexhowells <morganhowells314@gmail.com>
2022-07-30 21:50:04 +0100

    add simple telegram bot interface

---
+++ hexi/telegram/telegram.py
@@ -0,0 +1,25 @@
+import telebot
+
+def get_api_token():
+    with open("api.txt", "r") as apifile:
+        api_token = apifile.read().strip()
+
+    return api_token
+
+
+class TelegramBot:
+    def __init__(self, user_id):
+        self.bot = telebot.TeleBot(get_api_token())
+        self.user_id = user_id
+
+
+    def send_message(self, message):
+        self.bot.send_message(self.user_id, message)
+
+
+    def send_image(self, image):
+        self.bot.send_photo(self.user_id, image)
+
+
+    def send_video(self, video):
+        self.bot.send_video(self.user_id, video)
