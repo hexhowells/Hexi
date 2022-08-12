51d627ab91e266537266eb08045583ce75fa63d4
hexhowells <morganhowells314@gmail.com>
2022-08-12 20:34:04 +0100

    add whats my name skill

---
+++ hexi/core/skills/name/run.py
@@ -0,0 +1,24 @@
+# import interfaces here
+import time
+import threading
+from PIL import Image
+from hexi.interfaces.speaker import sound
+from hexi.interfaces.display import display
+
+
+# entry point of skill
+def start(command=None):
+    screen = display.Display()
+
+    t = threading.Thread(target=sound.play_wav, args=["name.wav"])
+    t.start()
+
+    face = Image.open("/home/pi/Hexi/hexi/assets/face/emotion1.png")
+    screen.show_image(face)
+    time.sleep(2)
+
+
+
+
+if __name__ == "__main__":
+    start()
