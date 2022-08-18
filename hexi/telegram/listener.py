86a1f05d86358679bc19f4853be2814e365c97eb
hexhowells <morganhowells314@gmail.com>
2022-08-18 13:16:18 +0100

    write messages to file and ping command

--- hexi/telegram/listener.py
+++ hexi/telegram/listener.py
@@ -1,5 +1,6 @@
 import telebot
 from hexi.auth import auth
+from datetime import datetime
 
 
 bot = telebot.TeleBot(auth.keys["telegram"])
@@ -7,18 +8,29 @@ bot = telebot.TeleBot(auth.keys["telegram"])
 
 # initial setup of user
 # check first if current user id exists
-@bot.message_handler(commands=['start'])
-def setup_user(message):
-    pass
+#@bot.message_handler(commands=['start'])
+#def setup_user(message):
+#    pass
 
 
-@bot.message_handler(commands=['wifi'])
-def wifi_setup(message):
-    pass
+
+@bot.message_handler(commands=['ping'])
+def ping(message):
+    now_raw = datetime.now()
+    now = now_raw.strftime("%d/%m/%Y %H:%M:%S")
+    msg = f'ping recieved at {now}'
+    bot.send_message(message.chat.id, msg)
+
 
 
 # recieve incoming messages
 # if message not from main user then store for user viewing
 @bot.message_handler(func=lambda message: True)
 def recieve_message(message):
-    pass
+    print("message recieved")
+    with open("messages.txt", "a") as msgfile:
+        msgfile.write(message.text + "\t\t")
+
+
+bot.infinity_polling()
+
