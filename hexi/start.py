59950a25b38cf1f9cbb3ea26f0037b09880d34da
hexhowells <morganhowells314@gmail.com>
2022-10-08 22:21:03 +0100

    add different faces to idle mode

--- hexi/start.py
+++ hexi/start.py
@@ -3,6 +3,9 @@ from PIL import Image
 import subprocess
 import os
 import sys
+import time
+import random
+from threading import Thread
 
 from speech.speech import STT
 from speech.wakeword import KeywordDetection
@@ -13,6 +16,44 @@ from hexi import config
 
 from core.intent.harpie import Harpie
 
+
+class PauseToken:
+    def __init__(self):
+        self.pause_state = False
+
+    def is_paused(self):
+        return self.pause_state
+
+    def pause(self):
+        print("paused")
+        self.pause_state = True
+
+    def unpause(self):
+        print("unpaused")
+        self.pause_state = False
+
+    def __str__(self):
+        return str(self.pause_state)
+
+
+def cycle_faces(token, screen):
+    sleep_face = Image.open("assets/face/sleep-face.png")
+    default_face = Image.open("assets/face/face.png")
+
+    while True:
+        if token.is_paused():
+            continue
+        else:
+            #random_time = random.randint(10, 120)
+            random_time = 5
+
+            time.sleep(random_time)
+            if not token.is_paused():
+                screen.show_image(sleep_face)
+
+            time.sleep(random_time)
+            if not token.is_paused():
+                screen.show_image(default_face)
 
 
 class Hexi:
@@ -28,6 +69,10 @@ class Hexi:
 
         self.config = config.load()
 
+        self.token = PauseToken()
+        t1 = Thread(target=cycle_faces, args=(self.token, self.screen,))
+        t1.start()
+
         subprocess.call(["boot/audio.sh"])
     
 
@@ -38,10 +83,14 @@ class Hexi:
     def idle(self):
         self._show_face()
         self.listener.run(self.listening_callback)
+        print("after callback")
         
 
     def listening_callback(self):
         print("keyword detected!")
+
+        self.token.pause()
+
         self._show_face(y=-5)
         sound.play_wav("assets/audio/beep.wav")
         command = self.stt.listen()
@@ -55,6 +104,7 @@ class Hexi:
 
         if len(intent) > 1:
             print("intent not found")
+            self.token.unpause()
         else:
             self.start_skill(intent, command)
         
@@ -76,6 +126,7 @@ class Hexi:
         
         sys.path.append(cur_dir)
         os.chdir(cur_dir)
+        self.token.unpause()
     
 
 
