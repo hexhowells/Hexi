0ad18916ecd028b068293931a87419db484c375f
hexhowells <morganhowells314@gmail.com>
2022-11-03 19:07:24 +0000

    add working message handler and start command line tools

--- hexi/telegram/listener.py
+++ hexi/telegram/listener.py
@@ -1,18 +1,27 @@
 import telebot
 from hexi.auth import auth
 from hexi.config import config
+from hexi.interfaces.display import display, icons
 from datetime import datetime
 
 
-bot = telebot.TeleBot(auth.keys["telegram"])
+class HexiTeleBot(telebot.TeleBot):
+    def __init__(self, *args, **kwargs):
+        self.pause_token = None
+        self.debug_on = False
+        self.screen = display.Display(font="fontawesome2.ttf")
+        super(HexiTeleBot, self).__init__(*args, **kwargs)
 
 
-# initial setup of user
-# check first if current user id exists
-#@bot.message_handler(commands=['start'])
-#def setup_user(message):
-#    pass
+bot = HexiTeleBot(auth.keys["telegram"])
+all_commands = ['help', 'debug', 'ping']
 
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    msg = "--- COMMANDS ---\n"
+    msg += '\n'.join(all_commands)
+    bot.send_message(message.chat.id, msg)
 
 
 @bot.message_handler(commands=['ping'])
@@ -23,24 +32,32 @@ def ping(message):
     bot.send_message(message.chat.id, msg)
 
 
+@bot.message_handler(commands=['debug'])
+def ping(message):
+    bot.debug_on = not bot.debug_on
+    if bot.debug_on:
+        if not bot.pause_token.is_paused():
+            bot.pause_token.pause()
+            bot.screen.show_icon(icons.Wrench)
+    else:
+        bot.pause_token.unpause()
 
-@bot.message_handler(commands=['blackout'])
-def blackout(message):
-    config_data = config.load()
-    config_data['blackout'] = not config_data['blackout']
-    config.save(config_data)
-    bot.send_message(message.chat.id, f'set blackout mode to {config_data["blackout"]}')
+    bot.send_message(message.chat.id, f"debug set to {bot.debug_on}")
 
 
-
-# recieve incoming messages
-# if message not from main user then store for user viewing
 @bot.message_handler(func=lambda message: True)
 def recieve_message(message):
-    print("message recieved")
-    with open("messages.txt", "a") as msgfile:
-        msgfile.write(message.text + "\t\t")
+    bot.pause_token.pause()
+    bot.screen.show_icon(icons.Envelope)
+    print(message.text)
+
+    bot.send_message(message.chat.id, "message recieved!")
+    with open("telegram/messages.txt", "w") as msg_file:
+        msg_file.write(message.text)
 
 
-bot.infinity_polling()
+
+def start_bot(pause_token):
+    bot.pause_token = pause_token
+    bot.infinity_polling()
 
