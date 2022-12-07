d8f46556c7d92e39192cdeff94dc470b91628367
hexhowells <morganhowells314@gmail.com>
2022-12-07 14:35:41 +0000

    add skill to display the battery level

---
+++ hexi/core/skills/battery/run.py
@@ -0,0 +1,23 @@
+# import interfaces here
+import time
+import sys
+from hexi.interfaces.battery import Battery
+from hexi.interfaces.display import display, icons
+
+
+# entry point of skill
+def start(command=None):
+    screen = display.Display()
+    battery = Battery()
+    level = battery.percentage()
+    battery_info = str(level) + "%"
+
+    screen.draw_text_custom(battery_info, 25, 15, 36)
+    time.sleep(5)
+
+
+
+if __name__ == "__main__":
+    command = sys.argv[1:]
+    if len(command) == 0: command = None
+    start(command)
