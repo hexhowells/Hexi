ca29f46ad2083d6af6dd1d7c3520e431f3fb0a7c
hexhowells <morganhowells314@gmail.com>
2022-12-13 17:31:08 +0000

    modify ambient to terminate on button press

--- hexi/core/skills/ambient/run.py
+++ hexi/core/skills/ambient/run.py
@@ -3,6 +3,9 @@ from hexi.interfaces.speaker import sound
 import rain
 import starfield
 import vlc
+import time
+import multiprocessing as mp
+from hexi.interfaces.button import Button
 
 
 # entry point of skill
@@ -12,9 +15,17 @@ def start(command=None):
     stream = vlc.MediaPlayer(audio_url)
     stream.play()
     
-    rain.start_animation()
-    #starfield.start_animation()
+    proc = mp.Process(target=rain.start_animation)
+    proc.start()
 
+    btn = Button()
+    while not btn.pushed():
+        time.sleep(0.1)
+
+    stream.stop()
+    proc.kill()
+
+    
 
 if __name__ == "__main__":
     start()
