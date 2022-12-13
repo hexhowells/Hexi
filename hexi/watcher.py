6785fdcf70c7b4294115f1deaf784619d5377781
hexhowells <morganhowells314@gmail.com>
2022-12-13 16:55:28 +0000

    add feature to reset core on button hold

--- hexi/watcher.py
+++ hexi/watcher.py
@@ -1,9 +1,20 @@
 import subprocess
 import time
+from hexi.interfaces.button import Button
+from threading import Thread
+
+
+def stop_core(proc):
+    print("killing core from watcher")
+    proc.kill()
 
 
 def start():
+    button = Button()
+
     proc = subprocess.Popen(["python3", "start.py"])
+    t1 = Thread(target=button.detect_hold, args=(stop_core, (proc, )))
+    t1.start()
     out, err = proc.communicate()
 
     print(f'[ERROR] - {err}')
