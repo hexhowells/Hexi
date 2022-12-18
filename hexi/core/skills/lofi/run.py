fbb522348e94d3149882202f163ac078a4bb358e
hexhowells <morganhowells314@gmail.com>
2022-12-18 17:52:26 +0000

    lower sound before playing

--- hexi/core/skills/lofi/run.py
+++ hexi/core/skills/lofi/run.py
@@ -1,17 +1,18 @@
 # import interfaces here
-from hexi.interfaces.speaker import sound
-#import rain
 import starfield
 import vlc
 import time
 import multiprocessing as mp
 from hexi.interfaces.button import Button
+from hexi.interfaces.speaker import sound
 
 
 # entry point of skill
 def start(command=None):
     audio_url = "http://lofi.stream.laut.fm/lofi?t302=2022-10-16_19-42-25&uuid=1be943bb-c5bf-486b-adb1-2c19e96c01dd"
-    
+   
+    #sound.set_volume(80)
+
     stream = vlc.MediaPlayer(audio_url)
     stream.play()
     
