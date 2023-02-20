831949f29704b73da4daeed308824ef61472bf41
hexhowells <morganhowells314@gmail.com>
2023-02-20 19:29:28 +0000

    add stop on button press

--- hexi/core/skills/dance/run.py
+++ hexi/core/skills/dance/run.py
@@ -1,6 +1,7 @@
 # import interfaces here
 from hexi.interfaces.speaker import sound
 from hexi.interfaces.motor import Motor
+from hexi.interfaces.button import Button
 
 from threading import Thread
 import multiprocessing
@@ -11,17 +12,22 @@ import random
 def dance(motor):
     seq_1 = [(Motor.RIGHT,0.5), (Motor.LEFT,0.5)]
     seq_2 = [(Motor.FORWARD,0.5), (Motor.BACKWARD,0.5)]
+    btn = Button()
 
     time.sleep(3)
 
     for i in range(6):
-        steps = random.randint(4, 7)
+        if btn.pushed(): break
+
+        steps = random.randint(1, 3)
         for i in range(steps):
             for direc, secs in seq_1:
+                if btn.pushed(): break
                 motor.drive(direc, secs)
                 time.sleep(0.38)
 
         for direc, secs in seq_2:
+            if btn.pushed(): break
             motor.drive(direc, secs)
             time.sleep(0.4)
 
@@ -29,16 +35,15 @@ def dance(motor):
 # entry point of skill
 def start(command=None):
     motor = Motor()
-    #music_process = multiprocessing.Process(target=sound.play_wav, args=("dance_music.wav", ))
-    #music_process.start()
+    audio = sound.Sound("dance_music.wav")
     
-    music_thread = Thread(target=sound.play_wav, args=("dance_music.wav", ))
+    music_thread = Thread(target=audio.play)
+#    music_thread = Thread(target=sound.play_wav, args=("dance_music.wav", ))
     music_thread.start()
     
-
     dance(motor)
-
-    music_process.terminate()
+    
+    audio.stop()
     
 
 if __name__ == "__main__":
